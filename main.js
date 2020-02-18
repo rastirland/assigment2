9de0b72b219d986f4a9cd791637704a7d01ab9df
rastirland@hotmail.com <rastirland@hotmail.com>
2020-02-18 11:27:10 +0000

    email test mobile

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
- 
- $( document ).ready(() => {
+
+$( document ).ready(() => {
  $(".dropdown-trigger").dropdown();
 
    let elems = document.querySelectorAll('.carousel');
@@ -20,4 +20,47 @@
     });
   });
 
-  
+  const email = document.getElementById("mail");
+
+  /*email.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+      email.setCustomValidity("I am expecting an e-mail address!");
+    } else {
+      email.setCustomValidity("");
+    }
+    
+  });*/
+
+	var form = document.forms["contact"];
+	form.addEventListener('submit',contact_submit,false);
+
+	function contact_submit(e) {
+		// Stop Form From Submitting
+		e.preventDefault();
+
+		// Set Initial Variables
+		let target = e.target || e.srcElement;
+		let to = 'rastirland@hotmail.com';
+		let uri = 'mailto:' + to;
+		let body = '';
+
+		// Set Form Values to Variables
+		let name = target.elements['name'].value;
+		let subject = target.elements['subject'].value;
+		let phone = target.elements['phone'].value;
+		let message = target.elements['message'].value;
+
+		// Build Body / Message with all Input Fields
+		body += message + "\r\n\r\n";
+		body += "Name: " + name + "\r\n";
+		body += "Phone Number: " + phone + "\r\n";
+
+		// Build final Mailto URI
+		uri += '?subject=' + encodeURIComponent(subject);
+		uri += '&body=' + encodeURIComponent(body);
+
+		// Open Mailto in New Window / Tab
+		window.open(uri,'_blank');
+	}
+
+
