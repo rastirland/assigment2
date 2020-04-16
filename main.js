56c56093bb822375f6895a424e8526a8b0722ae5
rastirland <rastirland@hotmail.com>
2020-04-16 09:11:12 +0100

    validation

--- main.js
+++ main.js
@@ -1,3 +1,81 @@
+function validation(){
+    let name = document.forms["contact"]["name"];
+    let subject = document.forms["contact"]["subject"];
+    let phone = document.forms["contact"]["phone"];
+    let message = document.forms["contact"]["message"];
+    
+
+    if (name.value == ""){
+        window.alert("Please enter name.");
+        name.focus();
+        return false;
+    }
+
+    else if (subject.value == ""){
+        window.alert("Please the put a message subject.");
+        subject.focus();
+        return false;
+    }
+
+    else if (phone.value == ""){
+        window.alert("Please enter your Number.");
+        phone.focus();
+        return false;
+    }
+
+    else if (message.value == ""){
+        window.alert("Please enter a message.");
+        message.focus();
+        return false;
+    }
+
+    else {
+    return true;
+    }
+};
+    function modalContacts(){
+        document.querySelector('.bg-modal').style.display= 'flex';
+    };
+
+function check(){
+    if (validation() == true){
+        modalContacts();
+        
+     }
+    };
+
+function contact_submit(e) {
+  // Stop Form From Submitting
+  e.preventDefault();
+
+  // Set Initial Variables
+  let target = e.target || e.srcElement;
+  let to = 'rastirland@hotmail.com';
+  let uri = 'mailto:' + to;
+  let body = '';
+
+  // Set Form Values to Variables
+  let name = target.elements['name'].value;
+  let subject = target.elements['subject'].value;
+  let phone = target.elements['phone'].value;
+  let message = target.elements['message'].value;
+
+  // Build Body / Message with all Input Fields
+  body += message + "\r\n\r\n";
+  body += "Name: " + name + "\r\n";
+  body += "Phone Number: " + phone + "\r\n";
+  
+  // Build final Mailto URI
+  uri += '?subject=' + encodeURIComponent(subject);
+  uri += '&body=' + encodeURIComponent(body);
+
+  // Open Mailto in New Window / Tab
+  window.open(uri,'_blank');
+}
+
+
+
+
 console.log("this is working")
 document.getElementById("menu").addEventListener("click", openMenu);
 function openMenu(){
@@ -63,34 +141,4 @@ $( document ).ready(() => {
 
 	let form = document.forms["contact"];
 	form.addEventListener('submit',contact_submit,false);
-
-	function contact_submit(e) {
-		// Stop Form From Submitting
-		e.preventDefault();
-
-		// Set Initial Variables
-		let target = e.target || e.srcElement;
-		let to = 'rastirland@hotmail.com';
-		let uri = 'mailto:' + to;
-		let body = '';
-
-		// Set Form Values to Variables
-		let name = target.elements['name'].value;
-		let subject = target.elements['subject'].value;
-		let phone = target.elements['phone'].value;
-		let message = target.elements['message'].value;
-
-		// Build Body / Message with all Input Fields
-		body += message + "\r\n\r\n";
-		body += "Name: " + name + "\r\n";
-		body += "Phone Number: " + phone + "\r\n";
-    
-		// Build final Mailto URI
-		uri += '?subject=' + encodeURIComponent(subject);
-		uri += '&body=' + encodeURIComponent(body);
-
-		// Open Mailto in New Window / Tab
-		window.open(uri,'_blank');
-	}
-
 
